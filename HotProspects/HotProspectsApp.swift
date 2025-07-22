9d79453796bb7932c598b0847bd727279ad6393b
BizMagnets <bizmagnets@BizMagnetss-Mac-mini.local>
2025-07-22 11:13:56 +0530

    inital commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by BizMagnets on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HotProspectsTests/HotProspectsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsTests.swift
+//  HotProspectsTests
+//
+//  Created by BizMagnets on 22/07/25.
+//
+
+import Testing
+@testable import HotProspects
+
+struct HotProspectsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
